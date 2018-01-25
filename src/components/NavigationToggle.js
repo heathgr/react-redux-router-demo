c96dc328c81b5003bd4335d2b0cc8b7fcb09d14d
Grant Heath <heathgr@gmail.com>
2018-01-25 01:40:17 -0700

    Added font awesome.  Still working on making things look prettyish.

--- src/components/NavigationToggle.js
+++ src/components/NavigationToggle.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const NavigationToggle = ({ toggleNavigationVisibility }) => (
-  <div onClick={() => toggleNavigationVisibility()} className='nav-toggle'>T</div>
+const NavigationToggle = ({ toggleNavigationVisibility, isNavigationVisible }) => (
+  <div onClick={() => toggleNavigationVisibility()} className={`nav-toggle ${isNavigationVisible && 'nav-toggle--close'}`}>
+    { !isNavigationVisible && <i class="fas fa-bars" /> }
+    { isNavigationVisible && <i class="fas fa-times" /> }
+  </div>
 )
 
 export default NavigationToggle;
